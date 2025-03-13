7bdd86f505678c0215ce2ae1d7dba260c890239f
sharipov <sharifzadehkhaled@gmail.com>
2025-03-13 13:58:56 +0500

    refactor: update database connection configuration and add Vercel deployment settings

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const Pool = require("pg").Pool;
 require("dotenv").config();
 
 const pool = new Pool({
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_DATABASE,
-});
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
 
 module.exports = pool;
